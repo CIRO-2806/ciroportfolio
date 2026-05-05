e3e91709ab488f5a95528883cc311415e0ae6e3b
CIRO-2806 <cirocabral2806@gmail.com>
2026-05-05 01:12:44 -0300

    estilo index

--- vista/anim.js
+++ vista/anim.js
@@ -1,52 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  const mainInfo = document.querySelector(".main-info");
-  const mainInfoText = document.querySelector(".main-info-h2")
+  const optionBar = document.querySelector(".option-bar")
+  const option = document.querySelectorAll(".option")
+
+  option.forEach((op, i) => {
+    setTimeout(() => {
+      op.style.transform = "translateY(0px)"
+    }, (i + 4) * 200);
+  });
+  setTimeout(() => {
+    optionBar.style.transform = "translateY(0px)"
+  }, 0);
 
 
-  setTimeout(() => {
-    mainInfo.style.transform = "translateY(0px)";
-  }, 50);
-  setTimeout(() => {
-    mainInfoText.style.transform = "translateY(0px)";
-    mainInfoText.style.opacity = "1";
-  }, 100);
-  setTimeout(() => {
-    mainInfoText.style.transform = "translateY(0px)";
-  }, 100);
+
 
 })
 const moreButton = document.getElementById("more-button")
-
-moreButton.addEventListener("click", toggleBoxes);
-
-const contactBox = document.querySelector(".contact")
-const linkImage = document.querySelector(".linkImage")
-contactBox.addEventListener("click", toggleContact);
-
-function toggleContact() {
-
-  contactBox.style.width = "15vw";
-
-  setTimeout(() => {
-    linkImage.style.opacity = "0";
-  }, 300)
-
-
-}
-function toggleBoxes() {
-  const moreSection = document.querySelector(".more");
-  const boxes = document.querySelectorAll(".more-box");
-
-  moreSection.style.display = "flex";
-  moreSection.style.zIndex = "999";
-
-  setTimeout(() => {
-    moreSection.style.opacity = "1";
-  }, 50);
-
-  boxes.forEach((box, index) => {
-    setTimeout(() => {
-      box.style.transform = "translateY(0)";
-    }, index * 100);
-  });
-}
